3066d5eed09f1d4f7271074350e7617bc4c4b56b
Keaton Turner <keaturner55@gmail.com>
2022-08-09 21:27:08 -0600

    added preprocessing to pipeline function

--- pre_proc.py
+++ pre_proc.py
@@ -5,27 +5,29 @@ def warn(*args, **kwargs):
 import warnings
 warnings.warn = warn
 
+#from sklearn.base import BaseEstimator, TransformerMixin
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer # tweet tokenizer is good for handling common words/symbols/emoticons found in social media data
 from nltk.corpus import stopwords
 from nltk.tag import pos_tag
-import dask.dataframe as ddf
+import pandas as pd
 import os
+from tqdm import tqdm
 
 #%%
-def clean_text(text):
+def clean_text(text_list):
     """
     Apply data cleaning pre-process
     """
     lemmatizer = WordNetLemmatizer()
     tokenizer = TweetTokenizer()
-    text_vector = []
-
-    #loop through input text list and apply transformations on individual words
-    for each_text in text:
+    stopwords_en=stopwords.words('english')
+    all_text = []
+    for each_text in text_list:
         lemmatized_tokens = []
         tokens=tokenizer.tokenize(each_text.lower())
         pos_tags=pos_tag(tokens)
+        # lemmatize each word/token based on its part of speach
         for each_token, tag in pos_tags: 
             if tag.startswith('NN'): 
                 pos='n'
@@ -34,15 +36,20 @@ def clean_text(text):
             else: 
                 pos='a'
             lemmatized_token=lemmatizer.lemmatize(each_token, pos)
-            lemmatized_tokens.append(lemmatized_token)
-        text_vector.append(' '.join(lemmatized_tokens))
-    return text_vector
+            if lemmatized_token not in stopwords_en:
+                lemmatized_tokens.append(lemmatized_token)
+        all_text.append(' '.join(lemmatized_tokens))
+    return all_text
+
 
 #%%
 if __name__=="__main__":
     DATA_DIR = 'C:\\Users\\keatu\\Regis_archive\\practicum2_data\\'
     colnames = ["target","id","date","flag","user","text"]
-    s140df = ddf.read_csv(os.path.join(DATA_DIR,"resources","s140_train.csv"), names = colnames, encoding='latin-1', blocksize=25e6)
+    print("reading csv")
+    s140df = pd.read_csv(os.path.join(DATA_DIR,"resources","s140_train.csv"), names = colnames, encoding='latin-1')
+    print("cleaning text")
     s140df['preproc_text'] = s140df['text'].apply(clean_text)
     s140df.to_csv(os.path.join(DATA_DIR,"s140_processed.csv"), index=False)
 # %%
+
